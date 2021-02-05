47457bb828abe002df6f1f2dd7aa17ce598a6d2e
tiagoamp <tiagoamp@gmail.com>
2021-02-05 16:01:44 -0300

    WIP: Initia implementation of fault tolerance with spring cloud circuitbreaker

--- shopping-cart/src/main/java/com/tiagoamp/shoppingcart/ShoppingCartApplication.java
+++ shopping-cart/src/main/java/com/tiagoamp/shoppingcart/ShoppingCartApplication.java
@@ -1,13 +1,20 @@
 package com.tiagoamp.shoppingcart;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+
+import java.time.Duration;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -28,6 +35,22 @@ public class ShoppingCartApplication {
 	@Bean
 	public WebClient.Builder getWeClientBuilder() {
 		return WebClient.builder();
+	}
+
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> globalCustomConfiguration() {
+		TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom()
+				.timeoutDuration(Duration.ofSeconds(5))
+				.build();
+		CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+				.failureRateThreshold(50)
+				.waitDurationInOpenState(Duration.ofMillis(2000))
+				.slidingWindowSize(2)
+				.build();
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.timeLimiterConfig(timeLimiterConfig)
+				.circuitBreakerConfig(circuitBreakerConfig)
+				.build());
 	}
 
 }
