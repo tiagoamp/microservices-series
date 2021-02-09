a463c88393a4b6f98c5ba8294f33fb5fa00bdf56
tiagoamp <tiagoamp@gmail.com>
2021-02-09 20:51:09 -0300

    Refactor services to use RepositoryRestResource

--- product-catalog/src/main/java/com/tiagoamp/productcatalog/repository/ProductRepository.java
+++ product-catalog/src/main/java/com/tiagoamp/productcatalog/repository/ProductRepository.java
@@ -1,9 +1,10 @@
 package com.tiagoamp.productcatalog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "product", path = "product")
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
 }
--- user-info/src/main/java/com/tiagoamp/userinfo/repository/UserRepository.java
+++ user-info/src/main/java/com/tiagoamp/userinfo/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.tiagoamp.userinfo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
 }
