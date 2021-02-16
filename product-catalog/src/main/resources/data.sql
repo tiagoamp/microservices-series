0d23d52d4611ab2dc247894d86ae9640c9ead8a0
tiagoamp <tiagoamp@gmail.com>
2021-02-16 18:55:04 -0300

    Fix text of insert

--- product-catalog/src/main/resources/data.sql
+++ product-catalog/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO PRODUCTS(ID, NAME, BRAND, DESCRIPTION, PRICE) VALUES (801, 'notebook','new brand', 'Super fast notebook', 2000.0);
+INSERT INTO PRODUCTS(ID, NAME, BRAND, DESCRIPTION, PRICE) VALUES (801, 'laptop','new brand', 'Super fast laptop', 2000.0);
 INSERT INTO PRODUCTS(ID, NAME, BRAND, DESCRIPTION, PRICE) VALUES (802, 'smartphone','no brand', 'black cellphone', 1500.0);
 INSERT INTO PRODUCTS(ID, NAME, BRAND, DESCRIPTION, PRICE) VALUES (803, 'video game','atari', 'old school video game', 1800.0);
 INSERT INTO PRODUCTS(ID, NAME, BRAND, DESCRIPTION, PRICE) VALUES (804, 'smart tv','any brand', 'Big smart tv full of apps', 2000.0);
